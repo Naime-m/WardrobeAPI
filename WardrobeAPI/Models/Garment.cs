84c05bf8ae0dc4781c261b49ea22f407f43d6a2e
Naime Medjek <naimemedjek@gmail.com>
2023-01-03 09:10:31 +0100

    added garment model

---
+++ WardrobeAPI/Models/Garment.cs
@@ -0,0 +1,10 @@
+﻿namespace WardrobeAPI.Models;
+
+public class Garment
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Color { get; set; }
+    public string? Description { get; set; }
+    public string Material { get; set; }
+}
