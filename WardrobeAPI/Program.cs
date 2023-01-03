e16f38fdf171caf8a5bfa6fa39f5326b8a12cd27
Naime Medjek <naimemedjek@gmail.com>
2023-01-03 09:27:04 +0100

    added connection string in program.cs

--- WardrobeAPI/Program.cs
+++ WardrobeAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WardrobeAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<GarmentDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("WardrobeDb")));
 
 var app = builder.Build();
 
