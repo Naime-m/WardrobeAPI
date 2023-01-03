d857f1c4f227a3b9f96ba00065d7da7ac292e01a
Naime Medjek <naimemedjek@gmail.com>
2023-01-03 09:20:19 +0100

    added efcore.tools, efcore.sqlserver and added dbcontext

---
+++ WardrobeAPI/Data/GarmentDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WardrobeAPI.Models;
+
+namespace WardrobeAPI.Data;
+
+public class GarmentDbContext : DbContext
+{
+	public GarmentDbContext(DbContextOptions<GarmentDbContext> options) : base(options)
+	{
+
+	}
+
+	DbSet<Garment> Garments {get; set; }
+}
