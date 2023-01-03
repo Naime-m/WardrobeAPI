80892474b193e9acda1fb0d00c85ce5789235400
Naime Medjek <naimemedjek@gmail.com>
2023-01-03 09:45:33 +0100

    scaffolded empty api controller

---
+++ WardrobeAPI/Controllers/GarmentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WardrobeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GarmentController : ControllerBase
+    {
+    }
+}
