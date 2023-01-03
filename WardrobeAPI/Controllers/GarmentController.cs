2bc5d53ed9fdbed4cfa1513b08de65fe77673c60
Naime Medjek <naimemedjek@gmail.com>
2023-01-03 09:48:52 +0100

    added dbcontext in controller constructor

--- WardrobeAPI/Controllers/GarmentController.cs
+++ WardrobeAPI/Controllers/GarmentController.cs
@@ -1,11 +1,18 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WardrobeAPI.Data;
 
-namespace WardrobeAPI.Controllers
+namespace WardrobeAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GarmentController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class GarmentController : ControllerBase
+    private readonly GarmentDbContext _context;
+
+    public GarmentController(GarmentDbContext context)
     {
+        _context = context;
     }
+
 }
